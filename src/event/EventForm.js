d40fbcc86fb9c525ee647c9d9b1e151614535231
DanFerrer <danielferrer212@gmail.com>
2017-01-20 03:09:39 -0500

    feature: added validation to EventForm component

--- src/event/EventForm.js
+++ src/event/EventForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
 import moment from 'moment';
+
 import guid from '../shared/guid';
 
 class EventForm extends Component {
@@ -20,6 +21,38 @@ class EventForm extends Component {
     this.onEndTimeChange = this.onEndTimeChange.bind(this);
     this.onTitleChange = this.onTitleChange.bind(this);
     this.onDescripitonChange = this.onDescripitonChange.bind(this);
+
+    this.validateTitle = this.validateTitle.bind(this);
+    this.validateStartTime = this.validateStartTime.bind(this);
+    this.validateEndTime = this.validateEndTime.bind(this);
+  }
+
+  validateTitle() {
+    return this.state.title.length ? 'success' : 'error';
+  }
+
+  validateStartTime() {
+    if (moment(this.state.start_time,'YYYY-MM-DD HH:mm:ss').isValid() && moment(this.state.start_time).valueOf() <= moment(this.state.end_time).valueOf() ) {
+      return 'success';
+    } else {
+      return 'error';
+    }
+  }
+
+  validateEndTime() {
+    if (moment(this.state.end_time, 'YYYY-MM-DD HH:mm:ss').isValid() && moment(this.state.end_time).valueOf() >= moment(this.state.start_time).valueOf()) {
+      return 'success';
+    } else {
+      return 'error';
+    }
+  }
+
+  formDisabled() {
+    if (this.validateTitle() === 'error' && this.validateStartTime() === 'error' && this.validateEndTime() === 'error') {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   onStartTimeChange(ev) {
@@ -41,22 +74,18 @@ class EventForm extends Component {
   onSubmit(ev) {
     ev.preventDefault();
 
-    const title = this.state.title;
-    const description = this.state.description;
-    const startTime = moment(this.state.start_time).format('YYYY-MM-DD HH:mm:ss');
-    const endTime = moment(this.state.end_time).format('YYYY-MM-DD HH:mm:ss');
-
     const newEvent = {
-      title,
-      description,
-      startTime,
-      endTime
+      id: this.state.id,
+      title: this.state.title,
+      description: this.state.description,
+      start_time: this.state.start_time,
+      end_time: this.state.end_time
     };
 
     this.props.addEvent(newEvent);
 
     this.setState({
-      id: '',
+      id: guid(),
       title: '',
       description: '',
       start_time: '',
@@ -64,49 +93,54 @@ class EventForm extends Component {
     });
  }
 
- render() {
-  return(
-    <form onSubmit={this.onSubmit}>
-      <FormGroup controlId='title'>
-        <ControlLabel>Event Title</ControlLabel>
-        <FormControl
-          type='text'
-          placeholder='Happy Birthday!'
-          value={this.state.title}
-          onChange={this.onTitleChange}
-        />
-      </FormGroup>
-      <FormGroup controlId='description'>
-        <ControlLabel>Description</ControlLabel>
-        <FormControl
-          componentClass='textarea'
-          placeholder="It\'s going to be so much fun!"
-          value={this.state.description}
-          onChange={this.onDescripitonChange}
-        />
-      </FormGroup>
-      <FormGroup controlId='start_time'>
-        <ControlLabel>Start Time</ControlLabel>
-        <FormControl
-          type='text'
-          placeholder='2017-12-13 15:30:00'
-          value={this.state.start_time}
-          onChange={this.onStartTimeChange}
-        />
-      </FormGroup>
-      <FormGroup controlId='end_time'>
-        <ControlLabel>End Time</ControlLabel>
-        <FormControl
-          type='text'
-          placeholder='2017-12-15 20:30:00'
-          value={this.state.end_time}
-          onChange={this.onEndTimeChange}
-        />
-      </FormGroup>
-      <Button bsStyle="primary" type="submit" value="Submit">Add Event</Button>
-    </form>
-  );
- }
+  render() {
+    let isDisabled = this.formDisabled();
+
+    return(
+      <form onSubmit={this.onSubmit}>
+        <FormGroup controlId='title' validationState={this.validateTitle()}>
+          <ControlLabel>Event Title</ControlLabel>
+          <FormControl
+            type='text'
+            placeholder='Happy Birthday!'
+            value={this.state.title}
+            onChange={this.onTitleChange}
+          />
+          <FormControl.Feedback />
+        </FormGroup>
+        <FormGroup controlId='description'>
+          <ControlLabel>Description</ControlLabel>
+          <FormControl
+            componentClass='textarea'
+            placeholder="It\'s going to be so much fun!"
+            value={this.state.description}
+            onChange={this.onDescripitonChange}
+          />
+        </FormGroup>
+        <FormGroup controlId='start_time' validationState={this.validateStartTime()}>
+          <ControlLabel>Start Time</ControlLabel>
+          <FormControl
+            type='text'
+            placeholder='2017-12-13 15:30:00'
+            value={this.state.start_time}
+            onChange={this.onStartTimeChange}
+          />
+          <FormControl.Feedback />
+        </FormGroup>
+        <FormGroup controlId='end_time' validationState={this.validateEndTime()}>
+          <ControlLabel>End Time</ControlLabel>
+          <FormControl
+            type='text'
+            placeholder='2017-12-15 20:30:00'
+            value={this.state.end_time}
+            onChange={this.onEndTimeChange}
+          />
+          <FormControl.Feedback />
+        </FormGroup>
+        <Button bsStyle="primary" type="submit" value="Submit" disabled={isDisabled}>Add Event</Button>
+      </form>
+    );
+  }
 }
 
 export default EventForm;
